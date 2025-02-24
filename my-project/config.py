2a4966dbef415586255195423eb95b68aca9b80c
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2025-02-24 13:25:00 +0100

    Add files via upload

---
+++ my-project/config.py
@@ -0,0 +1,31 @@
+# training parameters
+learning_rate = 0.002
+training_epochs = 100 #1500
+batch_size = 250              # the size of a minibatch
+test_display_step = 100       # how many epochs we do evaluate on the test set once
+save_step = 5                # how frequently do we save the model to disk
+
+# elastic deformation parameters
+sigma = 4
+alpha =20
+
+# lambda's are the ratios between the four error terms
+lambda_class = 20
+lambda_ae = 1
+lambda_1 = 1            # 1 and 2 here corresponds to the notation we used in the paper
+lambda_2 = 1
+
+# input data parameters
+input_height = 28         # MNIST data input shape
+input_width = input_height
+n_input_channel = 1       # the number of color channels; for MNIST is 1.
+input_size = input_height * input_width * n_input_channel   # the number of pixels in one input image
+input_shape = (1, n_input_channel, input_height, input_height) # input shape to pass in the model
+n_classes = 10
+
+# Network Parameters
+n_prototypes = 15         # the number of prototypes
+n_layers = 4
+n_maps = 32
+
+modeltype = 'standard' # 'standard'
