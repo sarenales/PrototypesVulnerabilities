43bdbf6c450262257193a08b687b21121399b0d8
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2025-02-28 12:03:10 +0100

    Add files via upload

---
+++ my-project2/loss_functions.py
@@ -0,0 +1,214 @@
+import torch
+import numpy as np
+from autoencoder_helpers import *
+
+def get_ms(prototype_classes, batch_y, change_expl=None):
+    examples_classes = batch_y.cpu().detach().numpy()
+    
+    # If force_class is integer, then we try to get closer to the prototype representing force_class
+    if isinstance(change_expl, int):
+        ms = torch.zeros((batch_y.shape[0], prototype_classes.shape[0]), dtype=torch.float)
+        
+        for e in range(examples_classes.shape[0]):
+            for p in range(prototype_classes.shape[0]):
+                if prototype_classes[p, change_expl] == 1:
+                    ms[e, p] = 1
+        return ms
+    
+    # If force_class is string, then we try to get closer to the prototype number force_class (P 'FC')
+    if isinstance(change_expl, str):  
+        ms = torch.zeros((batch_y.shape[0], prototype_classes.shape[0]), dtype=torch.float)      
+        ms[:, int(change_expl[1:])-1] = 1
+        return ms
+    
+    # If nothing, then we try to get away from the prototypes of the class of the example
+    ms = torch.zeros((batch_y.shape[0], prototype_classes.shape[0]), dtype=torch.float)
+    for e in range(examples_classes.shape[0]):
+        for p in range(prototype_classes.shape[0]):
+            if prototype_classes[p, examples_classes[e]] == 1:
+                ms[e, p] = 1 #0
+                
+    return ms
+
+# ADVL loss function
+def Loss_1(model, batch_x, batch_y, alpha1, alpha2, force_class, change_expl, objective):
+
+    """
+    if objective == "cenc": change expl no change class
+    if objective == cecc: change expl and change class
+    if objective == necl: no change explanation change class
+    """
+    
+    if objective == "necc" and change_expl is not None:
+        raise ValueError("If objective is necc, change_expl must be None")
+    if objective == "cenc" and force_class is None:
+        raise ValueError("If objective is cenc, force_class must be None")
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Define the device
+    
+    pred_y = model.forward(batch_x)
+    
+    loss_function = torch.nn.CrossEntropyLoss() # Cross-entropy loss
+    
+    # Boolean matrix, where each row is a prototype and if a column i has a 1, means that the prototype is from class i
+    thresh = -1.8 # We want to find prototypes for all the classes
+    prototype_classes = np.where((model.fc.linear.weight.cpu().detach().numpy().T <= np.min(model.fc.linear.weight.cpu().detach().numpy().T, axis=1, keepdims=True)) | (model.fc.linear.weight.cpu().detach().numpy().T < thresh), 1, 0)
+    
+    feature_vectors = model.feature_vectors # Latent representation of the examples in the batch_x
+    prototype_distances = model.prototype_layer.prototype_distances # Latent representation of the prototypes
+    
+    loss = 0
+      
+    ms = get_ms(prototype_classes, batch_y, change_expl=change_expl).to(device) # Importance matrix
+    
+    dists_loss = torch.sum(torch.matmul(ms, list_of_distances(prototype_distances, feature_vectors.view(-1, model.in_channels_prototype)))) # Weighted prototype distance loss
+    
+    if objective == "cenc": 
+        loss += -1 * alpha1 * loss_function(pred_y, batch_y)
+        
+        if change_expl is None:
+            loss += alpha2 * dists_loss
+            
+        elif change_expl is not None:
+            loss += -1 * alpha2 * dists_loss
+        
+    elif objective == "cecc":
+        
+        if force_class is None:
+            loss += alpha1 * loss_function(pred_y, batch_y)
+            
+        elif force_class is not None:
+            y_target = torch.full_like(batch_y, force_class)
+            loss += -1 * alpha1 * loss_function(pred_y, y_target)	
+        
+        if change_expl is None:
+            loss += alpha2 * dists_loss
+            
+        elif change_expl is not None:
+            loss += -1 * alpha2 * dists_loss
+      
+    elif objective == "necc":
+        
+        if force_class is None:
+            loss += alpha1 * loss_function(pred_y, batch_y)
+            
+        elif force_class is not None:
+            y_target = torch.full_like(batch_y, force_class)
+            loss += -1 * alpha1 * loss_function(pred_y, y_target)	
+        
+        loss += -1 * alpha2 * dists_loss
+
+    return loss
+
+def CELoss(model, batch_x, batch_y):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    loss_function = torch.nn.CrossEntropyLoss() # Cross-entropy loss
+    pred_y = model.forward(batch_x)
+    pred_y = pred_y.to(device)
+    return loss_function(pred_y, batch_y)
+
+"""
+    each row in d corresponds to one prototype and each column to one example. we want to maximize the distance between prototypes to
+    the examples that are not from the same class and minimize the distance between prototypes to the examples that are from the 
+    same class. 
+    
+    we create 2 masks, the clst mask, for each example, if the example is from class i, then the positions corresponding to the
+    distances between the prototypes of class i and the example are set to 1 and the rest to infinite. 
+    the sep mask is just the opposite.
+    
+    this way, after applying the mask the distances in wich we are interested in each case will have the corresponding value and the
+    rest infinite. beacuse we search for the minimun, the infnites are never going to be taken into account.
+    
+    """
+    
+def Clst_Sep_E1(model, batch_x, batch_y):
+    MAX = 1e5
+    feature_vectors = model.feature_vectors 
+    prototype_distances =  model.prototype_layer.prototype_distances 
+    n_prototypes = model.fc.linear.weight.size(1)
+    n_prototypes_by_class = n_prototypes // 10
+    
+    d = list_of_distances(prototype_distances, feature_vectors.view(-1, model.in_channels_prototype))
+    
+    mask_clst = torch.full_like(d, MAX)
+    mask_sep = torch.full_like(d, 1.0)
+    
+    for i in range(batch_x.shape[0]):
+        ini = batch_y[i] * n_prototypes_by_class
+        mask_clst[ini:ini + n_prototypes_by_class, i] = 1.0
+        mask_sep[ini:ini + n_prototypes_by_class, i] = MAX
+    
+    dists_to_class_prototypes = torch.mul(d, mask_clst)
+    dists_to_no_class_prototypes = torch.mul(d, mask_sep)
+    
+    clst = torch.mean(torch.min(dists_to_class_prototypes, dim=0)[0])  # minimum for each column
+    sep = -torch.mean(torch.min(dists_to_no_class_prototypes, dim=0)[0])  # minimum for each column
+    e1 = torch.mean(torch.min(dists_to_class_prototypes, dim=1)[0])
+    return clst, sep, e1
+
+
+#Adapted deafult loss replaicing train_e2 with clst and sep loss, for the balanced model
+def ClstSepLoss(model, batch_x, batch_y, pred_y, lambda_class, lambda_1, lambda_clus, lambda_sep, lambda_ae, batch_x_ori=None):
+    
+    loss_function = torch.nn.CrossEntropyLoss()
+    train_ce = loss_function(pred_y, batch_y)
+
+    feature_vectors = model.feature_vectors
+
+    clst_l, sep_l, train_e1 = Clst_Sep_E1(model, batch_x, batch_y)
+    
+    out_decoder = model.decoder(feature_vectors)
+    
+    if batch_x_ori is None:
+        train_ae = torch.mean(list_of_norms(out_decoder-batch_x))
+    else:
+        train_ae = torch.mean(list_of_norms(out_decoder-batch_x_ori))
+
+    train_te = lambda_class * train_ce +\
+            lambda_1 * train_e1 +\
+            lambda_clus * clst_l +\
+            lambda_sep * sep_l +\
+            lambda_ae * train_ae
+            
+    return train_te, train_ce, train_e1, clst_l, sep_l, train_ae
+
+
+#Loss used by default to train PrototypeDL
+def generalLoss(model, batch_x, batch_y, pred_y, lambda_class, lambda_1, lambda_2, lambda_ae, batch_x_ori=None):
+    
+    loss_function = torch.nn.CrossEntropyLoss()
+    train_ce = loss_function(pred_y, batch_y)
+
+    prototype_distances = model.prototype_layer.prototype_distances
+    feature_vectors = model.feature_vectors
+
+    if lambda_1 == 0:
+        train_e1 = 0
+    else:
+        train_e1 = torch.mean(torch.min(list_of_distances(prototype_distances, feature_vectors.view(-1, model.in_channels_prototype)), dim=1)[0])
+    
+    train_e2 = torch.mean(torch.min(list_of_distances(feature_vectors.view(-1, model.in_channels_prototype ), prototype_distances), dim=1)[0])
+    
+    out_decoder = model.decoder(feature_vectors)
+    
+    if batch_x_ori is None:
+        train_ae = torch.mean(list_of_norms(out_decoder-batch_x))
+    else:
+        train_ae = torch.mean(list_of_norms(out_decoder-batch_x_ori))
+    
+    train_ae = torch.mean(list_of_norms(out_decoder-batch_x))
+
+    train_te = lambda_class * train_ce +\
+            lambda_1 * train_e1 +\
+            lambda_2 * train_e2 +\
+            lambda_ae * train_ae
+    
+    return train_te, train_ce, train_e1, train_e2, train_ae
+
+
+    
+    
+    
+    
+    
+    
---
+++ my-project2/modules.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+from autoencoder_helpers import list_of_distances
+
+class EncoderLayer(nn.Module):
+    def __init__(self, in_channels=1, n_maps=32, out_channels=10, n_layers=4):
+        super(EncoderLayer, self).__init__()
+        
+        el = []
+        el += [ConvLayer(in_channels, n_maps, stride=2, padding=1)]
+        for i in range(0, n_layers-2):
+            el += [ConvLayer(n_maps, n_maps, stride=2, padding=1)]   
+        el += [ConvLayer(n_maps, out_channels, stride=2, padding=1)]
+
+        self.encoder = nn.Sequential(*el)
+
+    def forward(self, x):
+        return self.encoder(x)
+
+class DecoderLayer(nn.Module):
+    def __init__(self, in_channels=10, n_maps=32, out_channels=1, out_shapes=[], n_layers=4):
+        super(DecoderLayer, self).__init__()
+
+        dl = []
+        dl += [DeConvLayer(in_channels, n_maps, out_shape=out_shapes[-1], stride=2, padding=1, output_padding=1)]
+        for i in range(1, n_layers-1):
+            dl += [DeConvLayer(n_maps, n_maps, out_shape=out_shapes[-(i+1)], stride=2, padding=1, output_padding=1)]
+        dl += [DeConvLayer(n_maps, out_channels, out_shape=out_shapes[-n_layers], stride=2, padding=1, 
+                           output_padding=1, activation=nn.Sigmoid)]
+
+        self.decoder = nn.Sequential(*dl)
+
+    def forward(self, x):
+        return self.decoder(x)
+
+class PrototypeLayer(nn.Module):
+    def __init__(self, in_channels=10, n_prototypes=15):
+        super(PrototypeLayer, self).__init__()
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.prototype_distances = nn.Parameter(torch.rand(n_prototypes, in_channels, requires_grad=True).to(device))
+
+    def forward(self, x):
+        return list_of_distances(x, self.prototype_distances)
+
+class ConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=2, padding=1, activation=nn.ReLU):
+        super(ConvLayer, self).__init__()
+        
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3,
+                             stride=stride, padding=padding)
+        self.activation = activation()
+
+    def forward(self, x):
+        self.in_dim = x.shape[-2:]
+        return self.activation(self.conv(x)) 
+
+class DeConvLayer(nn.Module):
+    def __init__(self, in_channels, out_channels, out_shape, stride=2, padding=1, output_padding=1, activation=nn.ReLU):
+        super(DeConvLayer, self).__init__()
+        self.out_shape = out_shape
+        self.dconv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, 
+                                        stride=stride, padding=padding, output_padding=output_padding)
+        self.activation = activation()
+
+    def forward(self, x):
+        out_convt = self.dconv(x)
+        if (out_convt.shape[-2:][0] != self.out_shape[0]) & (out_convt.shape[-2:][1] != self.out_shape[1]):
+            out_convt = out_convt[:,:,(out_convt.shape[-2:][0] - self.out_shape[0]):,
+                                 (out_convt.shape[-2:][1] - self.out_shape[1]):]
+        return self.activation(out_convt)
+
+class FullyConnectedLayer(nn.Module):
+    def __init__(self, in_features=15, out_features=10):
+        super(FullyConnectedLayer, self).__init__()
+
+        self.linear = nn.Linear(in_features=in_features, out_features=out_features)
+
+    def forward(self, x):
+        return self.linear(x)
+
+class IdentityLayer(nn.Module):
+    def __init__(self, out_features=10, prototypes_by_class=3, fill_with=0.5):
+        super(IdentityLayer, self).__init__()
+        self.linear = nn.Linear(out_features * prototypes_by_class, out_features, bias=False)
+        
+        w = torch.full((out_features, out_features * prototypes_by_class), fill_with)
+        for i in range(out_features):
+            w[i, i * prototypes_by_class:(i + 1) * prototypes_by_class] = -1
+        self.linear.weight = nn.Parameter(w)  
+
+    def forward(self, x):
+        return self.linear(x)
+    
+class Softmax(nn.Module):
+    def __init__(self):
+        super(Softmax, self).__init__()
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        return self.softmax(x)
+
+# CAE Model definition 
+class CAEModel(nn.Module):
+    def __init__(self, input_shape=(1, 1, 28,28), n_maps=32, n_prototypes=15, n_layers=4, n_classes=10):
+        super(CAEModel, self).__init__()
+
+        # add encoder layer
+        self.encoder = EncoderLayer(in_channels=input_shape[1], n_maps=n_maps, out_channels=n_classes, n_layers=n_layers)
+        
+        # add prototype layer
+        self.in_channels_prototype = self.encoder.forward(torch.randn(input_shape)).view(-1,1).shape[0]
+        self.prototype_layer = PrototypeLayer(in_channels=self.in_channels_prototype, n_prototypes=n_prototypes)
+
+        # add decoder layer
+        decoder_out_shapes = []
+        for layer in self.encoder.modules():
+            if isinstance(layer, ConvLayer):
+                decoder_out_shapes += [list(layer.in_dim)]
+        self.decoder = DecoderLayer(in_channels=n_classes, n_maps=n_maps, out_channels=input_shape[1], out_shapes=decoder_out_shapes)
+        
+        # output layer
+        # final fully connected layer
+        self.fc = FullyConnectedLayer(in_features=n_prototypes, out_features=n_classes)
+        self.feature_vectors = None
+
+    def forward(self, x):
+        encoder_out = self.encoder(x)
+        self.feature_vectors = encoder_out
+        prototype_out = self.prototype_layer(encoder_out.view(-1,self.in_channels_prototype))
+        fc_out = self.fc(prototype_out)
+        return fc_out
+ 
+ # Balanced Model definition 
+class CAEModel_Balanced(nn.Module):
+    def __init__(self, input_shape=(1, 1, 28,28), n_maps=32, prototypes_by_class=3, n_layers=4, n_classes=10, fill_with=0.5):
+        super(CAEModel_Balanced, self).__init__()
+
+        # add encoder layer
+        self.encoder = EncoderLayer(in_channels=input_shape[1], n_maps=n_maps, out_channels=n_classes, n_layers=n_layers)
+        
+        # add prototype layer
+        self.in_channels_prototype = self.encoder.forward(torch.randn(input_shape)).view(-1,1).shape[0]
+        self.prototype_layer = PrototypeLayer(in_channels=self.in_channels_prototype, n_prototypes=int(prototypes_by_class*n_classes))
+
+        # add decoder layer
+        decoder_out_shapes = []
+        for layer in self.encoder.modules():
+            if isinstance(layer, ConvLayer):
+                decoder_out_shapes += [list(layer.in_dim)]
+        self.decoder = DecoderLayer(in_channels=n_classes, n_maps=n_maps, out_channels=input_shape[1], out_shapes=decoder_out_shapes)
+        
+        # output layer
+        # final fully connected layer
+        # CHANGED !! 
+        self.fc = IdentityLayer(prototypes_by_class=prototypes_by_class, out_features=n_classes, fill_with=fill_with)
+        self.feature_vectors = None
+
+    def forward(self, x):
+        encoder_out = self.encoder(x)
+        self.feature_vectors = encoder_out
+        prototype_out = self.prototype_layer(encoder_out.view(-1,self.in_channels_prototype))
+        fc_out = self.fc(prototype_out)
+        return fc_out
+       
